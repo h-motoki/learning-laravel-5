88b78c908c9024ef8602b46be3e9c6d5ea1c6230
ryo-ogata <ryousuke.ogata@gmail.com>
2015-02-19 01:56:54 +0900

    Add...

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,28 +8,18 @@ use Illuminate\Http\Request;
 class PagesController extends Controller {
 
 	public function about() {
-		$name = 'Jeffery <span style="color: red">Way</span>';
-		return view('pages.about')->with('name', $name);
+		$name = 'Jeffery Way';
+		// $people = [];
+		$people = [
+			'Taylor Otwell', 'Dayle Rees', 'Eric Barnes'
+		];
+		return view('pages.about')->with([
+			'name' => $name,
+			'people' => $people
+		]);
 	}
 
-	// public function about() {
-	// 	$name = 'Jeffery <span style="color: red">Way</span>';
-	// 	return view('pages.about')->with([
-	// 		'first' => 'Jeffery',
-	// 		'last' => 'Way'
-	// 	]);
-	// }
-
-	// public function about() {
-	// 	$data = [];
-	// 	$data['first'] = 'Jeffery';
-	// 	$data['last'] = 'Way';
-	// 	return view('pages.about', $data);
-	// }
-
-	// public function about() {
-	// 	$first = 'Jeffery';
-	// 	$last = 'Way';
-	// 	return view('pages.about', compact('first', 'last'));
-	// }
+	public function contact() {
+		return view('pages.contact');
+	}
 }
---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,13 @@
+@extends('app')
+
+@section('content')
+
+<h1>Contact Me!</h1>
+
+@stop
+
+@section('footer')
+
+<script type="text/javascript">alert('Contact from scripts!');</script>
+
+@stop
