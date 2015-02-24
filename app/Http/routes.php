2f54386e489ac217a6859fcf35aa3fc367637107
ryo-ogata <ryousuke.ogata@gmail.com>
2015-02-25 01:31:50 +0900

    Add show method

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 Route::get('articles', 'ArticlesController@index');
+Route::get('articles/{id}', 'ArticlesController@show');
 
 Route::get('about', 'PagesController@about');
 Route::get('contact', 'PagesController@contact');
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -5,7 +5,9 @@
 	<hr/>
 	@foreach ($articles as $article)
 		<article>
-			<h2>{{ $article->title }}</h2>
+			<h2>
+				<a href="{{ url('/articles', $article->id) }}">{{ $article->title }}</a>
+			</h2>
 			<div class="body">{{ $article->body }}</div>
 		</article>
 	@endforeach
