fc85357fd01d759f51f3e406de987929769ad235
ryo-ogata <ryousuke.ogata@gmail.com>
2015-03-01 19:57:56 +0900

    登録フォームを追加

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -3,19 +3,31 @@
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-use Illuminate\Http\Request;
+use Request;
 
 use App\Article;
+use Carbon\Carbon;
 
 class ArticlesController extends Controller {
 
 	public function index() {
-		$articles = Article::all();
+		$articles = Article::latest('published_at')->get();
 		return view('articles.index', compact('articles'));
 	}
 
 	public function show($id) {
 		$article = Article::findOrFail($id);
 		return view('articles.show', compact('article'));
+	}
+
+	public function create() {
+		return view('articles.create');
+	}
+
+	public function store() {
+		$input = Request::all();
+		$input['published_at'] = Carbon::now();
+		$article = Article::create($input);
+		return redirect('articles');
 	}
 }
