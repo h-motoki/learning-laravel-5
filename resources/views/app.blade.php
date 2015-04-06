237beab3ee62b07eb4d658c29ccc8cfe424de595
Ryosuke Ogata <r-ogata@fores.jp>
2015-04-06 14:42:39 +0900

    FlashScope Message

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,14 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
     <div class="container">
-        @yield('content')
+        @include('partials.flash')
     </div>
 
     @yield('footer')
 
+    <script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript">
+    	$('div.alert').not('.alert-important').delay(3000).slideUp(300);
+    </script>
 </body>
 </html>
