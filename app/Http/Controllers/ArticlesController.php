fd5804fcc4f8fd20128d581b179975a187355505
ryo-ogata <ryousuke.ogata@gmail.com>
2015-08-14 17:16:28 +0900

    bug fix ArticleController middleware

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -10,7 +10,7 @@ use Carbon\Carbon;
 class ArticlesController extends Controller {
 
 	public function __construct() {
-		$this->middleware('auth', ['except' => 'index', 'show']);
+		$this->middleware('auth', ['except' => ['index', 'show']]);
 	}
 
 	public function index() {
